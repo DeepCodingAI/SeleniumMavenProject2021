6efe0d1e9035b2066688db1fda11c4f798a3701d
mrahman <rahmanww@gmail.com>
2021-03-25 21:16:25 -0400

    maven project

---
+++ src/main/java/home/AmazonHomePage.java
@@ -0,0 +1,31 @@
+package home;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class AmazonHomePage {
+    public WebDriver driver = null;
+
+    @BeforeMethod
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver", "/Users/mrahman/develop/deepcoding/SeleniumMaven2021/driver/chromedriver");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://www.amazon.com/");
+        driver.manage().window().maximize();
+    }
+
+    @AfterMethod
+    public void cleanUp(){
+        driver.close();
+    }
+
+}
---
+++ src/test/java/landingpage/PageTitle.java
@@ -0,0 +1,16 @@
+package landingpage;
+
+import home.AmazonHomePage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PageTitle extends AmazonHomePage {
+
+    @Test
+    public void checkTitle(){
+        String expectedTitle = "Amazon.com: Online Shopping for Electronics, Apparel, Computers, Books, DVDs & more";
+        String actualTitle = driver.getTitle();
+        System.out.println(actualTitle);
+        Assert.assertEquals(expectedTitle,actualTitle);
+    }
+}
